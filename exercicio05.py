2dc9cd6f82503589edf62b2322e7e2063ece332f
Nano2001nano <netocaetano78@gmail.com>
2025-10-14 16:18:15 -0300

    lista

--- exercicio05.py
+++ exercicio05.py
@@ -3,6 +3,9 @@ escolha = input("Quantos títulos o piloto de fórmula 1 Lews Hamilton tem?\nDig
 
 if escolha == "3":
     print("\nResposta correta! Lewis Hamilton tem 7 títulos na Fórmula 1.")
+
+elif escolha != "1" and escolha != "2":
+    print("\nOpção inválida! Por favor, escolha 1, 2 ou 3.")
 else:
     print("\nResposta incorreta! Lewis Hamilton tem 7 títulos na Fórmula 1.")
 #estrutura condicional
